5f07a31cc91871ca200daa919dbd4ce9813e424d
caleb vergene <calebverg@gmail.com>
2024-06-26 11:56:49 -0700

    most starting page done

--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -28,18 +28,20 @@ const Toolbar = () => {
       }`}
     >
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg flex-1">My Toolbar</div>
+        <div className="text-white text-lg flex-1 font-medium">
+        <img src="/shlawp-pfp.png" className='w-10 h-auto'/>
+        </div>
         <nav className="flex-1 flex justify-center">
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-4 text-sm font-light">
             <li className="text-white">Home</li>
             <li className="text-white">About</li>
             <li className="text-white">Contact</li>
           </ul>
         </nav>
         <div className="flex-1 flex justify-end">
-          <button className="text-white bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded">
-            Action
-          </button>
+            <button className='bg-gradient-to-r from-fuchsia-600 to-orange-400 px-3 py-2 rounded-lg text-sm'>
+                Contact Us
+            </button>
         </div>
       </div>
     </div>
